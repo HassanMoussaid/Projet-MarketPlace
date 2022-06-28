3854631e367268dbbecb9efd43e7d3a4c2e7e8d4
ElieKara <elie.karabog@gmail.com>
2022-06-28 09:49:31 +0200

    Resource

--- src/main/java/com/m2i/configuration/AppConfig.java
+++ src/main/java/com/m2i/configuration/AppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
@@ -24,16 +25,24 @@ public class AppConfig {
 		InternalResourceViewResolver irvi = new InternalResourceViewResolver();
 		irvi.setPrefix("/WEB-INF/vues/");
 		irvi.setSuffix(".jsp");
+		
 		return irvi;
 	}
-
-	@Bean   
+	
+	
+	public void addResourceHandlers(ResourceHandlerRegistry rhr) {
+		 rhr.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		 // -> href=/resources/css/boostrap.css
+	}  
+	
+	@Bean  
 	public DataSource securityDataSource() {
 		// create connection pool        
 		ComboPooledDataSource securityDataSource  = new ComboPooledDataSource();
 		// set the jdbc driver class        
 		try {
 			securityDataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+			
 		} catch (PropertyVetoException exc) {
 			throw new RuntimeException(exc);
 		}
