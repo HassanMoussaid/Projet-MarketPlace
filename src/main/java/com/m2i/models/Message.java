6add281ae58cbef0dd9e397426a684fc4cb2ade5
ElieKara <elie.karabog@gmail.com>
2022-06-28 14:39:35 +0200

    Message

--- src/main/java/com/m2i/models/Message.java
+++ src/main/java/com/m2i/models/Message.java
@@ -1,5 +1,72 @@
 package com.m2i.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="messages")
 public class Message {
 
+	@Id
+	@GeneratedValue (strategy=GenerationType.IDENTITY)
+	@Column
+	private int id;
+	
+	@Column
+	private String message;
+	
+	@Column
+	private int idUserEnvoi;
+	
+	@Column
+	private int idUserRecoi;
+	
+	public Message() {
+		
+	}
+
+	public Message(String message, int idUserEnvoi, int idUserRecoi) {
+		super();
+		this.message = message;
+		this.idUserEnvoi = idUserEnvoi;
+		this.idUserRecoi = idUserRecoi;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getIdUserEnvoi() {
+		return idUserEnvoi;
+	}
+
+	public void setIdUserEnvoi(int idUserEnvoi) {
+		this.idUserEnvoi = idUserEnvoi;
+	}
+
+	public int getIdUserRecoi() {
+		return idUserRecoi;
+	}
+
+	public void setIdUserRecoi(int idUserRecoi) {
+		this.idUserRecoi = idUserRecoi;
+	}
+	
+	
 }
